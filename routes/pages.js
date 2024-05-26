b91ced3aa727bc8f312783a44b7ec4e669f029e5
Denariadna <149600221+Denariadna@users.noreply.github.com>
2024-05-26 19:00:06 +0300

    fix

--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,5 @@
 const pagesRouter = require("express").Router();
-const { sendIndex ,endDashboard } = require("../controllers/auth.js");
+const { sendIndex , sendDashboard } = require("../controllers/auth.js");
 const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 
 pagesRouter.get("/", sendIndex);
