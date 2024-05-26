2245dc36f556969a4a7d81940dfeb76bf1f80c94
Denariadna <149600221+Denariadna@users.noreply.github.com>
2024-05-26 18:46:57 +0300

    routing admins

--- routes/pages.js
+++ routes/pages.js
@@ -1,7 +1,8 @@
 const pagesRouter = require("express").Router();
-const { sendDashboard } = require("../controllers/auth.js");
+const { sendIndex ,endDashboard } = require("../controllers/auth.js");
 const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 
+pagesRouter.get("/", sendIndex);
 pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
 
 module.exports = pagesRouter;
